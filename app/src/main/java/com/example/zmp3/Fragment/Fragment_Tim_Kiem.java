d7ba6b1a02bd8c8845e9f73a6c7d54dcebc46e84
Tienduc37y <laitienduc37@gmail.com>
2023-04-14 08:17:04 +0700

    fragment search

---
+++ app/src/main/java/com/example/zmp3/Fragment/Fragment_Tim_Kiem.java
@@ -0,0 +1,44 @@
+package com.example.zmp3.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.zmp3.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Fragment_Tim_Kiem extends Fragment {
+
+    View view;
+    Toolbar toolbar;
+    RecyclerView recyclerViewsearchbaihat;
+    TextView txtkhongcodulieu;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_tim_kiem,container,false);
+        toolbar = view.findViewById(R.id.toolbarsearchbaihat);
+        recyclerViewsearchbaihat = view.findViewById(R.id.recyclerviewsearchbaihat);
+        txtkhongcodulieu = view.findViewById(R.id.textviewkhongcodulieu);
+      
+        return view;
+    }
+    }
