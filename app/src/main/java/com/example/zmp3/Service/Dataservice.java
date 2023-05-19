df2961d44b32cffd0900408c5e90cbd0d5e19491
Tienduc37y <laitienduc37@gmail.com>
2023-05-19 17:47:59 +0700

    update search bai hat

--- app/src/main/java/com/example/zmp3/Service/Dataservice.java
+++ app/src/main/java/com/example/zmp3/Service/Dataservice.java
@@ -1,20 +1,77 @@
 package com.example.zmp3.Service;
 
-import com.example.zmp3.Model.Quangcao;
+import com.example.zmp3.Model.Album;
+import com.example.zmp3.Model.Baihat;
+import com.example.zmp3.Model.ChuDe;
 import com.example.zmp3.Model.Playlist;
+import com.example.zmp3.Model.Quangcao;
+import com.example.zmp3.Model.TheLoai;
+import com.example.zmp3.Model.TheLoaiTrongNgay;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface Dataservice {
-
+    //phương thức thực hiện tương tác với dữ liệu
+    //tương tác vs sever = formUrEncoded
 
     @GET("songbanner.php")
     Call<List<Quangcao>> GetDataBanner();
 
+    @GET("playlistforcurrentday.php")
+    Call<List<Playlist>> GetPlayListCurrentDay();
+
+    @GET("chudevatheloaitrongngay.php")
+    Call<TheLoaiTrongNgay> GetDataChuDeVaTheLoai();
+
+    @GET("albumhot.php")
+    Call<List<Album>> GetAlbumHot();
+
+    @GET("baihatduocthich.php")
+    Call<List<Baihat>> GetBaiHatHot();
+
+    //tương tác vs sever = formUrEncoded
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDanhsachbaihattheoquangcao(@Field("idquangcao") String idquangcao);
+
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDanhsachbaihattheoplaylist(@Field("idplaylist") String idplaylist);
+
     @GET("danhsachcacplaylist.php")
     Call<List<Playlist>> GetDanhsachcacPlaylist();
 
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDanhsachbaihattheotheloai(@Field("idtheloai") String idtheloai);
+
+    @GET("tatcachude.php")
+    Call<List<ChuDe>> GetAllChuDe();
+
+
+    @FormUrlEncoded
+    @POST("theloaitheochude.php")
+    Call<List<TheLoai>> GetTheloaitheochude(@Field("idchude") String idchude);
+
+    @GET("tatcaalbum.php")
+    Call<List<Album>> GetAllAlbum();
+
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDanhsachbaihattheoalbum(@Field("idalbum") String idalbum);
+
+    @FormUrlEncoded
+    @POST("updateluotthich.php")
+    Call<String> UpdateLuotThich(@Field("luotthich") String luotthich,@Field("idbaihat") String idbaihat );
+
+    @FormUrlEncoded
+    @POST("searchbaihat.php")
+    Call<List<Baihat>> GetSearchBaiHat(@Field("tukhoa") String tukhoa);
 }
+
