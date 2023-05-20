e7f270f39fb6f66a53a68dc1ad5a230c5dc75510
Nkhanh <20010869@st.phenikaa-uni.edu.vn>
2023-05-20 23:19:12 +0700

    comment

---
+++ app/src/main/java/com/example/zmp3/Fragment/Fragment_Bai_Hat_Hot.java
@@ -0,0 +1,62 @@
+package com.example.zmp3.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.zmp3.Adapter.BaihathotAdapter;
+import com.example.zmp3.Model.Baihat;
+import com.example.zmp3.R;
+import com.example.zmp3.Service.APIService;
+import com.example.zmp3.Service.Dataservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Fragment_Bai_Hat_Hot extends Fragment {
+
+    View view;
+    RecyclerView recyclerViewbaihathot;
+    BaihathotAdapter baihathotAdapter;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_bai_hat_thich_nhat,container,false);
+        recyclerViewbaihathot = view.findViewById(R.id.recyclerviewbaihathot);
+        GetData();
+        return view;
+    }
+
+    private void GetData() {
+        Dataservice dataservice = APIService.getService();
+        Call<List<Baihat>> callback = dataservice.GetBaiHatHot();
+        callback.enqueue(new Callback<List<Baihat>>() {
+            @Override
+            public void onResponse(Call<List<Baihat>> call, Response<List<Baihat>> response) {
+                ArrayList<Baihat> baihatArrayList = (ArrayList<Baihat>) response.body();
+                baihathotAdapter = new BaihathotAdapter(getActivity(),baihatArrayList);
+                LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+                linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+                recyclerViewbaihathot.setLayoutManager(linearLayoutManager);
+                recyclerViewbaihathot.setAdapter(baihathotAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Baihat>> call, Throwable t) {
+
+            }
+        });
+    }
+}
