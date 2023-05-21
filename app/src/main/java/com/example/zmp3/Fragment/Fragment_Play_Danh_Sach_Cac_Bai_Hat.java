17aa79f81a777edb38f6159949705d29725a5c62
Nkhanh <20010869@st.phenikaa-uni.edu.vn>
2023-05-21 14:38:07 +0700

    update

---
+++ app/src/main/java/com/example/zmp3/Fragment/Fragment_Play_Danh_Sach_Cac_Bai_Hat.java
@@ -0,0 +1,35 @@
+package com.example.zmp3.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.zmp3.Activity.PlayNhacActivity;
+import com.example.zmp3.Adapter.PlaynhacAdapter;
+import com.example.zmp3.R;
+
+public class Fragment_Play_Danh_Sach_Cac_Bai_Hat extends Fragment {
+
+    View view;
+    RecyclerView recyclerViewplaynhac;
+    PlaynhacAdapter playnhacAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_play_danh_sach_cac_bai_hat,container,false);
+        recyclerViewplaynhac = view.findViewById(R.id.recyclerviewPlaybaihat);
+        if (PlayNhacActivity.mangbaihat.size()>0){
+            playnhacAdapter = new PlaynhacAdapter(getActivity(), PlayNhacActivity.mangbaihat);
+            recyclerViewplaynhac.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerViewplaynhac.setAdapter(playnhacAdapter);
+        }
+        return view;
+    }
+}
